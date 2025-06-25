d79874f42be25eb71520fdc8e5ad4e68a302c8d5
Riekert Luies <riekert@exah.co.za>
2025-06-25 09:01:38 +0200

    Force install pure python wheel

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 set -o errexit
 
 python3 -m pip install --upgrade pip
+
+# Force install the wheel file directly
+pip install ./chainlit-2.5.5+lc-cp312-cp312-manylinux_2_39_x86_64.whl --force-reinstall --no-deps
+
+# Install other requirements
 pip install -r requirements.txt -vvv
